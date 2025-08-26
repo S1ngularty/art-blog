998c5a365cc99d83f86c9d50552f22def60fcd3e
S1ngularty <asherxd10245@gmail.com>
2025-08-26 20:29:42 +0800

    divider

--- frontend/src/pages/Home/home.jsx
+++ frontend/src/pages/Home/home.jsx
@@ -25,6 +25,24 @@ function Home() {
           />
         </div>
       </Section>
+      <div className="divider"></div>
+        <Section sectionId={"section-1"}>
+        <div className="section-child" id="content-left">
+          <h1>Hello, I'm Levi</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. Consectetur
+            adipiscing elit quisque faucibus ex sapien vitae. Ex sapien vitae
+            pellentesque sem placerat in id. Placerat in id cursus mi pretium
+            tellus duis. Pretium tellus duis convallis tempus leo eu aenean.
+          </p>
+        </div>
+        <div className="section-child" id="content-right">
+          <img
+            key={"apollo-img"}
+            src="https://wallpapers.com/images/hd/apollo-belvedere-greek-statue-hlbd4e24ludtpu48.jpg"
+          />
+        </div>
+      </Section>
     </>
   );
 }
